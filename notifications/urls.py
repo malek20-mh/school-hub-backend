0121767395793aadf833d9f856705af0f368a321
Malek <zz775165375@gmail.com>
2025-12-29 01:46:08 +0300

    Fix import error in notifications urls

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import NotificationListView, MarkNotificationReadView
+from .views import NotificationListView, MarkNotificationReadView, MarkAllAsReadView
 
 urlpatterns = [
     path('', NotificationListView.as_view(), name='my-notifications'),
